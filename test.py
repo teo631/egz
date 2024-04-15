3e503ff18273d215cf642152e83bfc21a2c9a5a2
teo <teomanakcali01@gmail.com>
2024-04-16 00:06:13 +0300

    asdadsada

--- test.py
+++ test.py
@@ -18,9 +18,9 @@ def upload_image():
     image_file = request.files['image']
     bool_value1 = request.form.get('boolValue1')
     bool_value2 = request.form.get('boolValue2')
-    image_file.save("D:/frontEnd/resimler/uploaded_image.jpg")
+    image_file.save(os.getcwd()+"/resimler/uploaded_image.jpg")
 
-    x=hastaMi()
+    x=hastaMi(os.getcwd()+"/resimler/uploaded_image.jpg",bool_value1,bool_value2)
     print(bool_value2)
     if(bool_value1==bool_value2):
         x="adsa"
