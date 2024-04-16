1b1f6054de8bbc1571ec50f4ab26fe11b8712f89
teo <teomanakcali01@gmail.com>
2024-04-16 12:30:31 +0300

    asfdasd

--- post.js
+++ post.js
@@ -6,11 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
       const imageFile = document.getElementById('upload').files[0];
       formData.append('image', imageFile);
   
-      const boolValue1 = true;
-      const boolValue2 = false;
+      const boolValue1 = document.getElementById("k_v").value;
+      const boolValue2 = document.getElementById("bb_v").value;
+      const boolValue3 = document.getElementById("eklem_v").value;
+      const boolValue4 = document.getElementById("alerji_v").value;
+      const boolValue5 = document.getElementById("kabuk_v").value;
+      const boolValue6 = document.getElementById("sis_v").value;
       formData.append('boolValue1', boolValue1);
       formData.append('boolValue2', boolValue2);
-  
+      formData.append('boolValue3', boolValue3);
+      formData.append('boolValue4', boolValue4);
+      formData.append('boolValue5', boolValue5);
+      formData.append('boolValue6', boolValue6);
+
       fetch('http://127.0.0.1:5000/upload_image', {
         method: 'POST',
         body: formData,
--- test.py
+++ test.py
@@ -18,7 +18,14 @@ def upload_image():
     image_file = request.files['image']
     bool_value1 = request.form.get('boolValue1')
     bool_value2 = request.form.get('boolValue2')
+    bool_value3 = request.form.get('boolValue3')
+    bool_value4 = request.form.get('boolValue4')
+    bool_value5 = request.form.get('boolValue5')
+    bool_value6 = request.form.get('boolValue6')
+
     image_file.save(os.getcwd()+"/resimler/uploaded_image.jpg")
+
+    print(bool_value4)
 
     x=hastaMi(os.getcwd()+"/resimler/uploaded_image.jpg",bool_value1,bool_value2)
     print(bool_value2)
